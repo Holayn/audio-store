696b8ed17174566fddd3592ab91cd1075820f06c
Holayn <kwwong1597@gmail.com>
2021-06-30 21:03:34 -0400

    margin adjustment for track

--- packages/audio-store-web/src/components/Track.vue
+++ packages/audio-store-web/src/components/Track.vue
@@ -8,7 +8,7 @@
       class="mr-4 flex items-center justify-center rounded-full bg-black text-white">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
     </button>
-    <div class="max-h-12 overflow-auto">
+    <div class="flex-auto mr-1 max-h-12 overflow-auto">
       {{this.track.title}}
     </div>
     <div>
