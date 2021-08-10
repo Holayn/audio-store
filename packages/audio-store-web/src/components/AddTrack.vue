c84b7193e770d0d8ae7c730d4eeb16a68b803a48
Holayn <kwwong1597@gmail.com>
2021-08-10 00:17:45 -0400

    dont shift elements around when showing load indicator on add track

--- packages/audio-store-web/src/components/AddTrack.vue
+++ packages/audio-store-web/src/components/AddTrack.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="flex flex-col justify-center items-center w-full">
-    <div>
-      <input type="text" v-model="urlToLoad" class="border text-gray-900 p-1 rounded" placeholder="yt track/playlist url">
+  <div class="grid grid-rows-2 h-full w-full">
+    <div class="flex flex-col flex-auto justify-end items-center w-full">
+      <div>
+        <input type="text" v-model="urlToLoad" class="border text-gray-900 p-1 rounded" placeholder="yt track/playlist url">
+      </div>
+      <select class="mt-4 p-2 bg-gray-300 text-gray-600 w-1/2 rounded" v-model="playlistId">
+        <option :value="null" disabled>add to playlist...</option>
+        <option v-for="playlistId in playlistIds" :key="playlistId" :value="playlistId">
+          {{$store.getters.playlists[playlistId].title}}
+        </option>
+      </select>
+      <button
+        type="button"
+        @click="loadTrack()"
+        class="w-16 p-2 mt-4 flex items-center justify-center rounded-md bg-black text-white">
+        load
+      </button>
     </div>
-    <select class="mt-4 p-2 bg-gray-300 text-gray-600 w-1/2 rounded" v-model="playlistId">
-      <option :value="null" disabled>add to playlist...</option>
-      <option v-for="playlistId in playlistIds" :key="playlistId" :value="playlistId">
-        {{$store.getters.playlists[playlistId].title}}
-      </option>
-    </select>
-    <button
-      type="button"
-      @click="loadTrack()"
-      class="w-16 p-2 mt-4 flex items-center justify-center rounded-md bg-black text-white">
-      load
-    </button>
-    <div v-if="loading" class="flex justify-center pt-16">
-      <Loading/>
+    <div class="flex-auto">
+      <div class="flex justify-center pt-16">
+        <Loading v-if="loading"/>
+      </div>
     </div>
   </div>
 </template>
