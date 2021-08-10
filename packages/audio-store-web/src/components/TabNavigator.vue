b976370797f5463c9aab755232c160d893090d14
Holayn <kwwong1597@gmail.com>
2021-08-09 21:26:11 -0400

    currently selected tab border slightly darker

--- packages/audio-store-web/src/components/TabNavigator.vue
+++ packages/audio-store-web/src/components/TabNavigator.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-cols-2">
     <div
-      class="flex h-10 items-center justify-center cursor-pointer"
+      class="flex h-10 items-center justify-center cursor-pointer border-gray-300"
       :class="{
         'border-t-2': this.currentRouteName === 'Load'
       }"
@@ -12,7 +12,7 @@
       </div>
     </div>
     <div
-      class="flex h-10 items-center justify-center cursor-pointer"
+      class="flex h-10 items-center justify-center cursor-pointer border-gray-300"
       :class="{
         'border-t-2': this.currentRoutePath.match(/.*collection.*/)
       }"
