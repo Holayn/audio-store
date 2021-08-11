a4229a52f3b22fc6820b5a12f3bdb20dd3c04edb
Holayn <kwwong1597@gmail.com>
2021-08-10 20:24:39 -0400

    fix cut off playlist title

--- packages/audio-store-web/src/views/Playlist.vue
+++ packages/audio-store-web/src/views/Playlist.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col h-full">
-    <div class="text-2xl truncate">{{title}}</div>
+    <div class="flex flex-col justify-center p-2 text-2xl truncate">{{title}}</div>
     <div class="flex justify-center m-2">
       <svg @click="toggleMenu()" class="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
     </div>
