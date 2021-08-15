1ad69e31b9139e02c1769ce961400506356bc5a0
Holayn <kwwong1597@gmail.com>
2021-08-15 18:39:34 -0400

    use workbox-webpack-plugin instead for pwa

--- packages/audio-store-web/vue.config.js
+++ packages/audio-store-web/vue.config.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = {
   devServer: {
     https: true,
     key: fs.readFileSync(__dirname + '/192.168.0.133-key.pem', 'utf8'),
     cert: fs.readFileSync(__dirname + '/192.168.0.133.pem', 'utf8'),
-  }
+  },
+  configureWebpack: {
+    plugins: [
+      // Other plugins...
+      new GenerateSW(),
+    ],
+  },
 }
